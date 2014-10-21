24d191df7141841dfef2186a689b712c54d6c51f
Kallyn Gowdy <kallyngowdy@gmail.com>
2014-10-21 14:57:27 -0400

    Edited templates for emails and updated user account to not be so restrictive.

--- src/BrockAllen.MembershipReboot/Account/UserAccount.cs
+++ src/BrockAllen.MembershipReboot/Account/UserAccount.cs
@@ -11,61 +11,61 @@ namespace BrockAllen.MembershipReboot
 {
     public abstract class UserAccount
     {
-        public virtual Guid ID { get; protected internal set; }
+        public virtual Guid ID { get; set; }
 
         [StringLength(50)]
         [Required]
-        public virtual string Tenant { get; protected internal set; }
+        public virtual string Tenant { get; set; }
         
         // If email address is being used as the username then this property
         // should adhere to maximim length constraint for valid email addresses.
         // See Dominic Sayers answer at SO: http://stackoverflow.com/a/574698/99240
         [StringLength(254)]
         [Required]
-        public virtual string Username { get; protected internal set; }
+        public virtual string Username { get; set; }
 
-        public virtual DateTime Created { get; protected internal set; }
-        public virtual DateTime LastUpdated { get; protected internal set; }
-        public virtual bool IsAccountClosed { get; protected internal set; }
-        public virtual DateTime? AccountClosed { get; protected internal set; }
+        public virtual DateTime Created { get; set; }
+        public virtual DateTime LastUpdated { get; set; }
+        public virtual bool IsAccountClosed { get; set; }
+        public virtual DateTime? AccountClosed { get; set; }
 
-        public virtual bool IsLoginAllowed { get; protected internal set; }
-        public virtual DateTime? LastLogin { get; protected internal set; }
-        public virtual DateTime? LastFailedLogin { get; protected internal set; }
-        public virtual int FailedLoginCount { get; protected internal set; }
+        public virtual bool IsLoginAllowed { get;  set; }
+        public virtual DateTime? LastLogin { get;  set; }
+        public virtual DateTime? LastFailedLogin { get; set; }
+        public virtual int FailedLoginCount { get; set; }
 
-        public virtual DateTime? PasswordChanged { get; protected internal set; }
-        public virtual bool RequiresPasswordReset { get; protected internal set; }
+        public virtual DateTime? PasswordChanged { get; set; }
+        public virtual bool RequiresPasswordReset { get; set; }
 
         // Maximum length of a valid email address is 254 characters.
         // See Dominic Sayers answer at SO: http://stackoverflow.com/a/574698/99240
         [EmailAddress]
         [StringLength(254)]
-        public virtual string Email { get; protected internal set; }
-        public virtual bool IsAccountVerified { get; protected internal set; }
+        public virtual string Email { get; set; }
+        public virtual bool IsAccountVerified { get; set; }
 
-        public virtual DateTime? LastFailedPasswordReset { get; protected internal set; }
-        public virtual int FailedPasswordResetCount { get; protected internal set; }
+        public virtual DateTime? LastFailedPasswordReset { get; set; }
+        public virtual int FailedPasswordResetCount { get; set; }
 
         [StringLength(100)]
-        public virtual string MobileCode { get; protected internal set; }
-        public virtual DateTime? MobileCodeSent { get; protected internal set; }
+        public virtual string MobileCode { get; set; }
+        public virtual DateTime? MobileCodeSent { get; set; }
         [StringLength(20)]
-        public virtual string MobilePhoneNumber { get; protected internal set; }
-        public virtual DateTime? MobilePhoneNumberChanged { get; protected internal set; }
+        public virtual string MobilePhoneNumber { get; set; }
+        public virtual DateTime? MobilePhoneNumberChanged { get; set; }
 
-        public virtual TwoFactorAuthMode AccountTwoFactorAuthMode { get; protected internal set; }
-        public virtual TwoFactorAuthMode CurrentTwoFactorAuthStatus { get; protected internal set; }
+        public virtual TwoFactorAuthMode AccountTwoFactorAuthMode { get; set; }
+        public virtual TwoFactorAuthMode CurrentTwoFactorAuthStatus { get; set; }
 
         [StringLength(100)]
-        public virtual string VerificationKey { get; protected internal set; }
-        public virtual VerificationKeyPurpose? VerificationPurpose { get; protected internal set; }
-        public virtual DateTime? VerificationKeySent { get; protected internal set; }
+        public virtual string VerificationKey { get; set; }
+        public virtual VerificationKeyPurpose? VerificationPurpose { get; set; }
+        public virtual DateTime? VerificationKeySent { get; set; }
         [StringLength(100)]
-        public virtual string VerificationStorage { get; protected internal set; }
+        public virtual string VerificationStorage { get; set; }
 
         [StringLength(200)]
-        public virtual string HashedPassword { get; protected internal set; }
+        public virtual string HashedPassword { get; set; }
 
         public abstract IEnumerable<UserClaim> Claims { get; }
         protected internal abstract void AddClaim(UserClaim item);
